e8a232da8caafbd177bb019ecf51dc3d75931202
drobilica <dusan@drobilica.com>
2023-12-01 04:03:11 +0100

    Beautify featured flights

--- advanced-flight-system/src/components/FeaturedFlights.js
+++ advanced-flight-system/src/components/FeaturedFlights.js
@@ -1,58 +1,66 @@
 // src/components/FeaturedFlights.js
-
 import React, { useState, useEffect, useCallback } from "react";
-import * as styles from "./FeaturedFlights.module.css";
+import { Link } from "gatsby"; // Import Link from Gatsby
 import featuredFlightsData from "../data/featuredFlights.json";
-import moment from 'moment'; // Import Moment.js
+import moment from 'moment';
+import { Card, CardContent, Typography, IconButton, Box, Grid } from '@mui/material';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const FeaturedFlights = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const flightsToShow = 2; // Number of flights to show at a time
+  const flightsToShow = 2; 
   const totalFlights = featuredFlightsData.flights.length;
 
-  // Define nextFlight function with useCallback to memoize it
   const nextFlight = useCallback(() => {
     setCurrentIndex((currentIndex + 1) % (totalFlights - flightsToShow + 1));
   }, [currentIndex, totalFlights, flightsToShow]);
 
   const prevFlight = () => {
-    setCurrentIndex(prevIndex => {
-      // Calculate new index
-      let newIndex = prevIndex - 1;
-      // If the new index is less than 0, loop back to the end
-      if (newIndex < 0) {
-        newIndex = totalFlights - flightsToShow;
-      }
-      return newIndex;
-    });
+    setCurrentIndex(prevIndex => prevIndex - 1 < 0 ? totalFlights - flightsToShow : prevIndex - 1);
   };
 
   useEffect(() => {
-    const interval = setInterval(nextFlight, 5000); // Change flight every 5 seconds
-    return () => clearInterval(interval); // Cleanup interval on unmount
-  }, [nextFlight]); // Include nextFlight in dependency array
+    const interval = setInterval(nextFlight, 5000);
+    return () => clearInterval(interval);
+  }, [nextFlight]);
 
   return (
-    <div className={styles.featuredFlights}>
-      <h2>Featured Flights</h2>
-      <button onClick={prevFlight} className={styles.arrowButton}>{"<"}</button>
-      <div className={styles.flightsContainer}>
-        {[...Array(flightsToShow)].map((_, i) => {
-          const flightIndex = (currentIndex + i) % totalFlights;
-          const flight = featuredFlightsData.flights[flightIndex];
-          return (
-            <div key={flight.id} className={styles.flightCard}>
-              <h3>{flight.airline} - {flight.departureAirport} to {flight.arrivalAirport}</h3>
-              <p>Departure: {moment(flight.departureTime).format('HH:mm, DD MMMM YYYY')}</p><p> Arrival: {moment(flight.arrivalTime).format('HH:mm, DD MMMM YYYY')}</p>
-              <p>Duration: {flight.duration} | Distance: {flight.distance}</p>
-              <p>Class: {flight.class} | Price: ${flight.price}</p>
-              <p>Seats Available: {flight.availableSeats}</p>
-            </div>
-          );
-        })}
-      </div>
-      <button onClick={nextFlight} className={styles.arrowButton}>{">"}</button>
-    </div>
+    <Box sx={{ textAlign: 'center', my: 4 }}>
+      <Typography variant="h4" gutterBottom>Featured Flights</Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <IconButton onClick={prevFlight} color="primary">
+          <ArrowBackIosIcon />
+        </IconButton>
+        <Grid container spacing={2} justifyContent="center">
+          {[...Array(flightsToShow)].map((_, i) => {
+            const flightIndex = (currentIndex + i) % totalFlights;
+            const flight = featuredFlightsData.flights[flightIndex];
+            return (
+              <Grid item key={flight.id} xs={12} sm={6}>
+                <Link to={`/details/${flight.id}`} style={{ textDecoration: 'none' }}> {/* Link to the flight details page */}
+                  <Card raised>
+                    <CardContent>
+                      <Typography variant="h5" gutterBottom>
+                        {flight.airline} - {flight.departureAirport} to {flight.arrivalAirport}
+                      </Typography>
+                      <Typography variant="body1">Departure: {moment(flight.departureTime).format('HH:mm, DD MMMM YYYY')}</Typography>
+                      <Typography variant="body1">Arrival: {moment(flight.arrivalTime).format('HH:mm, DD MMMM YYYY')}</Typography>
+                      <Typography variant="body1">Duration: {flight.duration} | Distance: {flight.distance}</Typography>
+                      <Typography variant="body1">Class: {flight.class} | Price: ${flight.price}</Typography>
+                      <Typography variant="body1">Seats Available: {flight.availableSeats}</Typography>
+                    </CardContent>
+                  </Card>
+                </Link>
+              </Grid>
+            );
+          })}
+        </Grid>
+        <IconButton onClick={nextFlight} color="primary">
+          <ArrowForwardIosIcon />
+        </IconButton>
+      </Box>
+    </Box>
   );
 };
 
